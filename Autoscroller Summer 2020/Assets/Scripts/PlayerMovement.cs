4744bb22d042e17b46b11f4f4003b98fc0ed38ea
kylefwalters <kylefwalters@gmail.com>
2020-05-12 15:22:16 -0400

    addding Unity stuff

---
+++ Autoscroller Summer 2020/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float movementScale = 1;
+
+    void Start()
+    {
+        CharacterController character = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        float velocityX = Input.GetAxis("Horizontal") * movementScale;
+        float velocityY = Input.GetAxis("Vertical") * movementScale;
+
+
+    }
+}
