aab3aac1fede33c69d440c72134cddcd545949c3
BIGnP <64586604+bignp1@users.noreply.github.com>
2020-05-13 20:26:02 -0400

    Add files via upload

---
+++ Autoscroller Summer 2020/Assets/Scripts/ObserverScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ObserverScript : MonoBehaviour
+{
+    public static ObserverScript Instance { get; private set; }
+    public int bulletSelector;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
