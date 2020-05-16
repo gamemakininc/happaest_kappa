767e421413194edf379f18eb6300e3bf76387627
BIGnP <64586604+bignp1@users.noreply.github.com>
2020-05-15 22:18:48 -0400

    Add files via upload

---
+++ Autoscroller Summer 2020/Assets/Scripts/IDragHandeler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class IDragHandeler : MonoBehaviour, IDragHandeler, IEndDragHandeler
+{
+	public void OnDrag(PointerEventData eventData) 
+	{
+		transform.position = Input.mousePosition;
+	}
+	public void OnEndDrag(PointerEventData eventData) 
+	{
+		transform.position = Vector3.zero;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Autoscroller Summer 2020/Assets/Scripts/fittingScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class fittingScript : MonoBehaviour 
+{
+	//set bace values
+	public float baceMoveSpeed;
+	public int bacebulletSelector;
+	public int bacePayload0Selector;
+	public int bacePayload1Selector;
+	public int baceHealth;
+	//swap files
+	public float MoveSpeed;
+	public int bulletSelector;
+	public int payload0Selector;
+	public int payload1Selector;
+	public int health;
+	//the secret sauce
+	public int[] fitSetup;
+	public void Start() 
+	{
+		fitSetup = ObserverScript.Instance.fitSetup;
+
+	}
+	public void SetIcons() 
+	{
+		//set icons in the wheel 
+	}
+	public void SetOutputs()
+	{
+		//aka wall of if statements
+
+	}
+	public void Update() 
+	{
+
+	}
+
+}
