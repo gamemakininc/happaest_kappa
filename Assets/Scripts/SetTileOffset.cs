b50aaa54de36ddc248a0353bae0daaac4abbe744
kylefwalters <kylefwalters@gmail.com>
2020-06-06 23:34:28 -0400

    Tile Scripts

---
+++ Assets/Scripts/SetTileOffset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SetTileOffset : MonoBehaviour
+{
+    //Use this to offset tile so that it spawns on the edge of the camera
+    void Awake()
+    {
+        transform.position = transform.position + new Vector3(GetComponent<SpriteRenderer>().bounds.extents.x, 0, 0);
+    }
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-1, 0);
+    }
+}
