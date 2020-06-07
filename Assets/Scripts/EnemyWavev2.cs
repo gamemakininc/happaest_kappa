769159d971abef2b95d838b3775bbe6904aef4a9
kylefwalters <kylefwalters@gmail.com>
2020-06-06 23:33:48 -0400

    Wavespawner added

--- Assets/Scripts/EnemyWavev2.cs
+++ Assets/Scripts/EnemyWavev2.cs
@@ -24,6 +24,7 @@ public class EnemyWavev2 : MonoBehaviour
 
     private float cameraBoundX;
     private float tileEdgeX;
+    private GameObject _selectedWave;
 
     public enum states
     {
@@ -40,19 +41,22 @@ public class EnemyWavev2 : MonoBehaviour
         remainingElites = eliteWaves;
         _Boss = Boss;
         _tilePool = new List<GameObject>(tilePool);
-        _elitePool = new List<GameObject>(_elitePool);
+        _elitePool = new List<GameObject>(elitePool);
         //Right border of the camera
         cameraBoundX = Camera.main.ViewportToWorldPoint(new Vector3(1, 0.5f, 0)).x;
 
-        GameObject _selectedWave;
         SelectWave(out _selectedWave);
-        currentTile = Instantiate(_selectedWave, new Vector2(cameraBoundX, 0), Quaternion.Euler(0.0f, 0.0f, 0.0f));
+        Debug.Log("Spawning Wave");
+        currentTile = Instantiate(_selectedWave, new Vector2(cameraBoundX, 0), Quaternion.Euler(0.0f, 0.0f, 0.0f)).transform.GetChild(0).gameObject;
 
         currentState = states.spawning;
+
+        Debug.Log("Tile bounds.extents = " + currentTile.GetComponent<SpriteRenderer>().bounds.extents.x);
     }
 
     private void Update()
     {
+        waveCount = remainingWaves; //Debug
         if (currentTile != null)
         {
             //The right border of the background tile
@@ -62,12 +66,22 @@ public class EnemyWavev2 : MonoBehaviour
         switch (currentState) //States
         {
             case states.spawning:
+                if(remainingWaves == 0 && remainingElites == 0)
+                {
+                    currentState = states.boss;
+                    return;
+                }
                 float xDif = tileEdgeX - cameraBoundX; //The distance between the camera's right border and the right edge of the tile
+                //Debug.Log("xDif = " + xDif);
                 if(xDif < float.Epsilon)
                 {
-                    GameObject _selectedWave;
+                    //Debug.Log("Spawning Wave");
                     SelectWave(out _selectedWave);
-                    currentTile = Instantiate(_selectedWave, Vector3.zero, Quaternion.Euler(0.0f, 0.0f, 0.0f));
+                    currentTile = Instantiate(_selectedWave, new Vector2(cameraBoundX, 0), Quaternion.Euler(0.0f, 0.0f, 0.0f)).transform.GetChild(0).gameObject;
+                    return;
+
+                    //tileEdgeX = currentTile.transform.position.x + currentTile.GetComponent<SpriteRenderer>().bounds.extents.x;
+                    //xDif = tileEdgeX - cameraBoundX;
                 }
                 break;
             case states.paused:
@@ -77,6 +91,10 @@ public class EnemyWavev2 : MonoBehaviour
                 {
                     Instantiate(_Boss, Vector3.zero, Quaternion.Euler(0.0f, 0.0f, 0.0f));
                     _Boss = null;
+                }
+                else
+                {
+                    currentState = states.win;
                 }
                 break;
             case states.win:
@@ -91,22 +109,25 @@ public class EnemyWavev2 : MonoBehaviour
     void SelectWave(out GameObject selectedWave) 
     {
         int totalWaves = _tilePool.Count + _elitePool.Count;
+        //Debug.Log(totalWaves);
         int randNum = Mathf.RoundToInt(Random.Range(0, totalWaves));
         //prevents the same tile being used twice
-        while(randNum == previousTile)
+        while(randNum == previousTile || randNum > _tilePool.Count && remainingElites == 0 || randNum < _tilePool.Count && remainingWaves == 0)
             randNum = Mathf.RoundToInt(Random.Range(0, totalWaves));
         previousTile = randNum;
         if (randNum > _tilePool.Count)
         {
+            Debug.Log("Spawn Elite");
             randNum -= _tilePool.Count;
             selectedWave = _elitePool[randNum];
-            _elitePool.Remove(selectedWave);
+            //_elitePool.Remove(selectedWave);
             remainingElites--;
         }
         else
         {
+            Debug.Log("Spawn Wave");
             selectedWave = _tilePool[randNum];
-            _tilePool.Remove(selectedWave);
+            //_tilePool.Remove(selectedWave);
             remainingWaves--;
         }
         return;
@@ -114,6 +135,7 @@ public class EnemyWavev2 : MonoBehaviour
 
     void OnLevelComplete() //Runs when currentState = states.win
     {
+        Debug.Log("Level Complete");
         currentState = states.paused;
     }
 }
