4643443a91cd1b78953f321f95c8ed1d0b3dd009
bignp <bignp999@yahoo.com>
2020-06-19 22:09:51 -0400

    some horrible code

--- Assets/Scripts/Gameplay/boss/sbosstracker.cs
+++ Assets/Scripts/Gameplay/boss/sbosstracker.cs
@@ -4,12 +4,16 @@ public class sbosstracker : MonoBehaviour
 {
     //array of active thrusters
     public bool[] thrusters;
+    public GameObject[] thrusterObj;
     //do not use to calculate health as lasers cant hit thrusters.
     private int thrustIntMax;
     private int thrustIntCurrent;
     private int thCounter;
     //array of active turrets
     public bool[] turrets;
+    public GameObject[] laserObj;
+    public GameObject[] cannonObj;
+    public GameObject[] missileObj;
     //used for health calculation
     private int turretsMax;
     private int turretsCurrent;
@@ -19,7 +23,16 @@ public class sbosstracker : MonoBehaviour
     public int tInputInt;//handel for thruster script
     private int swapint;
     private int counter;
-    // Start is called before the first frame update
+
+    //weapon timers
+    private float thrustTimer;
+    private float laserTimer;
+    private float cannonTimer;
+    private float missileTimer;
+    //selecter counter
+    private int selecterCounter;
+    private float lazorCounter;
+    // Start is called before the first frame update 
     void Start()
     {
         thrustIntMax = thrusters.Length;
@@ -70,6 +83,218 @@ public class sbosstracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //run timers
+        missileTimer += 1 * Time.deltaTime; 
+        cannonTimer += 1 * Time.deltaTime; 
+        laserTimer += 1 * Time.deltaTime; 
+        thrustTimer += 1 * Time.deltaTime;
+        selecterCounter = 0;
+        //fire first volly
+        if (laserTimer >= 1 && laserTimer <= 1.5 && laserObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (laserObj[selecterCounter] == null) 
+        {
+            if (laserObj[1] != null || laserObj[2] != null || laserObj[3] != null || laserObj[4] != null || laserObj[5] != null || laserObj[6] != null || laserObj[7] != null || laserObj[8] != null || laserObj[9] != null || laserObj[10] != null) {/*do nothing*/ }
+            else { laserTimer = 0; }
+        }
+        if (missileTimer >= selecterCounter+1.5 && missileTimer <= selecterCounter+2 && missileObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (missileObj[selecterCounter] == null) 
+        {
+            if (missileObj[1] != null || missileObj[2] != null || missileObj[3] != null || missileObj[4] != null || missileObj[5] != null || missileObj[6] != null || missileObj[7] != null || missileObj[8] != null || missileObj[9] != null || missileObj[10] != null) {/*do nothing*/ }
+            else { missileTimer = 0; }
+        }
+        if (cannonTimer >= selecterCounter+2 && cannonTimer <= selecterCounter+2.5 && cannonObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (cannonObj[selecterCounter] == null) 
+        {
+            if (cannonObj[1] != null || cannonObj[2] != null || cannonObj[3] != null || cannonObj[4] != null || cannonObj[5] != null || cannonObj[6] != null || cannonObj[7] != null || cannonObj[8] != null || cannonObj[9] != null || cannonObj[10] != null) {/*do nothing*/ }
+            else { cannonTimer = 0; }
+        }
+        selecterCounter++;
+        lazorCounter += 3;
+        //fire second volly
+        if (laserTimer >= lazorCounter && laserTimer <= lazorCounter+0.5 && laserObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (laserObj[selecterCounter] == null)
+        {
+            if (laserObj[2] != null || laserObj[3] != null || laserObj[4] != null || laserObj[5] != null || laserObj[6] != null || laserObj[7] != null || laserObj[8] != null || laserObj[9] != null || laserObj[10] != null) {/*do nothing*/ }
+            else { laserTimer = 0; }
+        }
+        if (missileTimer >= selecterCounter + 1.5 && missileTimer <= selecterCounter + 2 && missileObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (missileObj[selecterCounter] == null)
+        {
+            if ( missileObj[2] != null || missileObj[3] != null || missileObj[4] != null || missileObj[5] != null || missileObj[6] != null || missileObj[7] != null || missileObj[8] != null || missileObj[9] != null || missileObj[10] != null) {/*do nothing*/ }
+            else { missileTimer = 0; }
+        }
+        if (cannonTimer >= selecterCounter + 2 && cannonTimer <= selecterCounter + 2.5 && cannonObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (cannonObj[selecterCounter] == null)
+        {
+            if ( cannonObj[2] != null || cannonObj[3] != null || cannonObj[4] != null || cannonObj[5] != null || cannonObj[6] != null || cannonObj[7] != null || cannonObj[8] != null || cannonObj[9] != null || cannonObj[10] != null) {/*do nothing*/ }
+            else { cannonTimer = 0; }
+        }
+        selecterCounter++;
+        lazorCounter += 3;
+        //fire third volly
+        if (laserTimer >= lazorCounter && laserTimer <= lazorCounter + 0.5 && laserObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (laserObj[selecterCounter] == null)
+        {
+            if ( laserObj[3] != null || laserObj[4] != null || laserObj[5] != null || laserObj[6] != null || laserObj[7] != null || laserObj[8] != null || laserObj[9] != null || laserObj[10] != null) {/*do nothing*/ }
+            else { laserTimer = 0; }
+        }
+        if (missileTimer >= selecterCounter + 1.5 && missileTimer <= selecterCounter + 2 && missileObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (missileObj[selecterCounter] == null)
+        {
+            if ( missileObj[3] != null || missileObj[4] != null || missileObj[5] != null || missileObj[6] != null || missileObj[7] != null || missileObj[8] != null || missileObj[9] != null || missileObj[10] != null) {/*do nothing*/ }
+            else { missileTimer = 0; }
+        }
+        if (cannonTimer >= selecterCounter + 2 && cannonTimer <= selecterCounter + 2.5 && cannonObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (cannonObj[selecterCounter] == null)
+        {
+            if ( cannonObj[3] != null || cannonObj[4] != null || cannonObj[5] != null || cannonObj[6] != null || cannonObj[7] != null || cannonObj[8] != null || cannonObj[9] != null || cannonObj[10] != null) {/*do nothing*/ }
+            else { cannonTimer = 0; }
+        }
+        selecterCounter++;
+        lazorCounter += 3;
+        //fire fourth volly
+        if (laserTimer >= lazorCounter && laserTimer <= lazorCounter + 0.5 && laserObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (laserObj[selecterCounter] == null)
+        {
+            if ( laserObj[4] != null || laserObj[5] != null || laserObj[6] != null || laserObj[7] != null || laserObj[8] != null || laserObj[9] != null || laserObj[10] != null) {/*do nothing*/ }
+            else { laserTimer = 0; }
+        }
+        if (missileTimer >= selecterCounter + 1.5 && missileTimer <= selecterCounter + 2 && missileObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (missileObj[selecterCounter] == null)
+        {
+            if ( missileObj[4] != null || missileObj[5] != null || missileObj[6] != null || missileObj[7] != null || missileObj[8] != null || missileObj[9] != null || missileObj[10] != null) {/*do nothing*/ }
+            else { missileTimer = 0; }
+        }
+        if (cannonTimer >= selecterCounter + 2 && cannonTimer <= selecterCounter + 2.5 && cannonObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (cannonObj[selecterCounter] == null)
+        {
+            if ( cannonObj[4] != null || cannonObj[5] != null || cannonObj[6] != null || cannonObj[7] != null || cannonObj[8] != null || cannonObj[9] != null || cannonObj[10] != null) {/*do nothing*/ }
+            else { cannonTimer = 0; }
+        }
+        selecterCounter++;
+        lazorCounter += 3;
+        //fire fifth volly
+        if (laserTimer >= lazorCounter && laserTimer <= lazorCounter + 0.5 && laserObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (laserObj[selecterCounter] == null)
+        {
+            if ( laserObj[5] != null || laserObj[6] != null || laserObj[7] != null || laserObj[8] != null || laserObj[9] != null || laserObj[10] != null) {/*do nothing*/ }
+            else { laserTimer = 0; }
+        }
+        if (missileTimer >= selecterCounter + 1.5 && missileTimer <= selecterCounter + 2 && missileObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (missileObj[selecterCounter] == null)
+        {
+            if ( missileObj[5] != null || missileObj[6] != null || missileObj[7] != null || missileObj[8] != null || missileObj[9] != null || missileObj[10] != null) {/*do nothing*/ }
+            else { missileTimer = 0; }
+        }
+        if (cannonTimer >= selecterCounter + 2 && cannonTimer <= selecterCounter + 2.5 && cannonObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (cannonObj[selecterCounter] == null)
+        {
+            if ( cannonObj[5] != null || cannonObj[6] != null || cannonObj[7] != null || cannonObj[8] != null || cannonObj[9] != null || cannonObj[10] != null) {/*do nothing*/ }
+            else { cannonTimer = 0; }
+        }
+        selecterCounter++;
+        lazorCounter += 3;
+        //fire sixth volly
+        if (laserTimer >= lazorCounter && laserTimer <= lazorCounter + 0.5 && laserObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (laserObj[selecterCounter] == null)
+        {
+            if ( laserObj[6] != null || laserObj[7] != null || laserObj[8] != null || laserObj[9] != null || laserObj[10] != null) {/*do nothing*/ }
+            else { laserTimer = 0; }
+        }
+        if (missileTimer >= selecterCounter + 1.5 && missileTimer <= selecterCounter + 2 && missileObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (missileObj[selecterCounter] == null)
+        {
+            if ( missileObj[6] != null || missileObj[7] != null || missileObj[8] != null || missileObj[9] != null || missileObj[10] != null) {/*do nothing*/ }
+            else { missileTimer = 0; }
+        }
+        if (cannonTimer >= selecterCounter + 2 && cannonTimer <= selecterCounter + 2.5 && cannonObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (cannonObj[selecterCounter] == null)
+        {
+            if ( cannonObj[6] != null || cannonObj[7] != null || cannonObj[8] != null || cannonObj[9] != null || cannonObj[10] != null) {/*do nothing*/ }
+            else { cannonTimer = 0; }
+        }
+        selecterCounter++;
+        lazorCounter += 3;
+        //fire seventh volly
+        if (laserTimer >= lazorCounter && laserTimer <= lazorCounter + 0.5 && laserObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (laserObj[selecterCounter] == null)
+        {
+            if ( laserObj[7] != null || laserObj[8] != null || laserObj[9] != null || laserObj[10] != null) {/*do nothing*/ }
+            else { laserTimer = 0; }
+        }
+        if (missileTimer >= selecterCounter + 1.5 && missileTimer <= selecterCounter + 2 && missileObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (missileObj[selecterCounter] == null)
+        {
+            if ( missileObj[7] != null || missileObj[8] != null || missileObj[9] != null || missileObj[10] != null) {/*do nothing*/ }
+            else { missileTimer = 0; }
+        }
+        if (cannonTimer >= selecterCounter + 2 && cannonTimer <= selecterCounter + 2.5 && cannonObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (cannonObj[selecterCounter] == null)
+        {
+            if ( cannonObj[7] != null || cannonObj[8] != null || cannonObj[9] != null || cannonObj[10] != null) {/*do nothing*/ }
+            else { cannonTimer = 0; }
+        }
+        selecterCounter++;
+        lazorCounter += 3;
+        //fire eaghth volly
+        if (laserTimer >= lazorCounter && laserTimer <= lazorCounter + 0.5 && laserObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (laserObj[selecterCounter] == null)
+        {
+            if ( laserObj[8] != null || laserObj[9] != null || laserObj[10] != null) {/*do nothing*/ }
+            else { laserTimer = 0; }
+        }
+        if (missileTimer >= selecterCounter + 1.5 && missileTimer <= selecterCounter + 2 && missileObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (missileObj[selecterCounter] == null)
+        {
+            if ( missileObj[8] != null || missileObj[9] != null || missileObj[10] != null) {/*do nothing*/ }
+            else { missileTimer = 0; }
+        }
+        if (cannonTimer >= selecterCounter + 2 && cannonTimer <= selecterCounter + 2.5 && cannonObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (cannonObj[selecterCounter] == null)
+        {
+            if ( cannonObj[8] != null || cannonObj[9] != null || cannonObj[10] != null) {/*do nothing*/ }
+            else { cannonTimer = 0; }
+        }
+        selecterCounter++;
+        lazorCounter += 3;
+        //fire nineth volly
+        if (laserTimer >= lazorCounter && laserTimer <= lazorCounter + 0.5 && laserObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (laserObj[selecterCounter] == null)
+        {
+            if ( laserObj[9] != null || laserObj[10] != null) {/*do nothing*/ }
+            else { laserTimer = 0; }
+        }
+        if (missileTimer >= selecterCounter + 1.5 && missileTimer <= selecterCounter + 2 && missileObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (missileObj[selecterCounter] == null)
+        {
+            if ( missileObj[9] != null || missileObj[10] != null) {/*do nothing*/ }
+            else { missileTimer = 0; }
+        }
+        if (cannonTimer >= selecterCounter + 2 && cannonTimer <= selecterCounter + 2.5 && cannonObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (cannonObj[selecterCounter] == null)
+        {
+            if ( cannonObj[9] != null || cannonObj[10] != null) {/*do nothing*/ }
+            else { cannonTimer = 0; }
+        }
+        selecterCounter++;
+        lazorCounter += 3;
+        //fire tenth volly
+        if (laserTimer >= lazorCounter && laserTimer <= lazorCounter + 0.5 && laserObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (laserObj[selecterCounter] == null)
+        {
+            if (laserObj[9] != null || laserObj[10] != null) {/*do nothing*/ }
+            else { laserTimer = 0; }
+        }
+        if (missileTimer >= selecterCounter + 1.5 && missileTimer <= selecterCounter + 2 && missileObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (missileObj[selecterCounter] == null)
+        { missileTimer = 0; }
+        if (cannonTimer >= selecterCounter + 2 && cannonTimer <= selecterCounter + 2.5 && cannonObj[selecterCounter] != null) { laserObj[selecterCounter].GetComponent<turretScript>().fire(); }
+        else if (cannonObj[selecterCounter] == null)
+        {cannonTimer = 0; }
+        selecterCounter++;
+        lazorCounter += 3;
+
+
+
     }
 }
