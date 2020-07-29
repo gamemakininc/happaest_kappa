c9025003fa3d0bd7b01f699937c21b5e73f632d9
kylefwalters <kylefwalters@gmail.com>
2020-07-29 14:02:27 -0400

    Shaders

---
+++ Assets/Scripts/PostProcessing/Shaders/Glitch Shader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(GlitchShaderRenderer), PostProcessEvent.AfterStack, "Custom/Glitch Shader")]
+public sealed class GlitchShader : PostProcessEffectSettings
+{
+    [Range(0,1), Tooltip("Amount of color drift")]
+    public FloatParameter drift = new FloatParameter { value = 0.08f };
+    [Range(0,1), Tooltip("Amount of horizontal jitter")]
+    public FloatParameter jitter = new FloatParameter { value = 0.2f };
+    public TextureParameter trashTex = new TextureParameter();
+}
+
+public sealed class GlitchShaderRenderer : PostProcessEffectRenderer<GlitchShader>
+{
+    public override void Render(PostProcessRenderContext context)
+    {
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Glitch Shader"));
+        sheet.properties.SetFloat("_Drift", settings.drift);
+        sheet.properties.SetFloat("_Jitter", settings.jitter);
+        sheet.properties.SetTexture("_TrashTex", settings.trashTex.value);
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
---
+++ Assets/Scripts/PostProcessing/Shaders/Glitch Shader.shader
@@ -0,0 +1,42 @@
+﻿Shader "Hidden/Custom/Glitch Shader"
+{
+	HLSLINCLUDE
+
+		#include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+
+		TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+		TEXTURE2D_SAMPLER2D(_TrashTex, sampler_TrashTex);
+
+		float _Drift;
+		float _Jitter;
+		//Texture2D _TrashTex;
+		
+		float4 Frag(VaryingsDefault i) : SV_Target
+		{
+			float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
+
+			float4 offset = SAMPLE_TEXTURE2D(_TrashTex, sampler_TrashTex, i.texcoord);
+
+			half4 src1 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, float2(i.texcoord.x - (_Drift / 30) + ((offset.x - 0.5) * _Jitter / 30), i.texcoord.y));
+			half4 src2 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, float2(i.texcoord.x + (_Drift / 30) + ((offset.x - 0.5) * _Jitter / 30), i.texcoord.y));
+
+			return half4(src1.r, src2.g, src1.b, 1);
+		}
+	
+	ENDHLSL
+
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            HLSLPROGRAM
+            #pragma vertex VertDefault
+            #pragma fragment Frag
+
+			ENDHLSL
+        }
+    }
+}
