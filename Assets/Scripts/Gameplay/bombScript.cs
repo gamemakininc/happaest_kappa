c173cd8b5be9bc83d75d199a2887232572671ea6
bignp <bignp999@yahoo.com>
2021-04-25 03:26:17 -0400

    sync attempt

---
+++ Assets/Scripts/Gameplay/bombScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class bombScript : MonoBehaviour
+{
+    public GameObject flare;
+    public GameObject body;
+    public Transform point;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
